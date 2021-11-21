c01fe97ba760b34db37eff160882b2c9705e86f9
Stephen Darcy <stephendarcy123@gmail.com>
2021-11-21 22:29:55 +0000

    initial generation done

--- Assignment/Assets/Scripts/GenerateTerrain.cs
+++ Assignment/Assets/Scripts/GenerateTerrain.cs
@@ -5,7 +5,7 @@ public class GenerateTerrain : MonoBehaviour
     // Exposing to unity
     [SerializeField] private int width = 256;
     [SerializeField] private int height = 256;
-    [SerializeField] private int depth = 10;
+    [SerializeField] private int depth = 30;
     [SerializeField] private float scale = 10f;
     [SerializeField] private float xOffset;
     [SerializeField] private float yOffset;
@@ -23,12 +23,12 @@ public class GenerateTerrain : MonoBehaviour
     void BuildTerrain()
     {
         // Generating random offset so terrain will be random each time
-        xOffset = Random.Range(0, 100);
-        yOffset = Random.Range(0, 100);
+        xOffset = Random.Range(0, 10000);
+        yOffset = Random.Range(0, 10000);
 
         // Storing the current terrain data in 'data' and then updating it with new values
         data = terrain.terrainData;
-        data.heightmapResolution = width;
+        data.heightmapResolution = width + 1;
         data.size = new Vector3(width, depth, height);
         data.SetHeights(0, 0, GetHeights());
 
@@ -42,7 +42,7 @@ public class GenerateTerrain : MonoBehaviour
         float[,] heights = new float[width, height];
         for (int i = 0; i < width; i++)
         {
-            for (int j = 0; j < depth; j++)
+            for (int j = 0; j < height; j++)
             {
                 heights[i, j] = CalculateHeights(i, j);
             }
@@ -55,7 +55,7 @@ public class GenerateTerrain : MonoBehaviour
     float CalculateHeights(int x, int y)
     {
         float xCoordinate = ((float)x / width) * scale + xOffset;
-        float yCoordinate = ((float)x / height) * scale + yOffset;
+        float yCoordinate = ((float)y / height) * scale + yOffset;
 
         return Mathf.PerlinNoise(xCoordinate, yCoordinate);
     }
