bdf8c3ca6052c1aba7e1fe8583268749bdf707b4
Stephen Darcy <stephendarcy123@gmail.com>
2021-12-14 22:41:52 +0000

    start of highscore script

--- Assignment/Assets/Scripts/score.cs
+++ Assignment/Assets/Scripts/score.cs
@@ -6,7 +6,9 @@ public class score : MonoBehaviour
 
     public Transform player;
     public Text scoreValue;
+    public Text highscoreText;
     public float distance;
+    public float currentHighscore;
     public float previousDistance = 0f;
 
     // Update is called once per frame
@@ -16,6 +18,15 @@ public class score : MonoBehaviour
         if (distance > previousDistance) {
             scoreValue.text = distance.ToString("0");
             previousDistance = distance;
+            currentHighscore = distance;
         }
+
+        float highScore = PlayerPrefs.GetFloat("highScore");
+        highscoreText.text = highScore.ToString("0");
+
+        if (currentHighscore > highScore) {
+            PlayerPrefs.SetFloat("highScore", currentHighscore);
+        }
+
     }
 }
