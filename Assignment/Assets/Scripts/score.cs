bc6e8e7f3782aa45a4bcdc2bab8a1d4a37111785
Stephen Darcy <stephendarcy123@gmail.com>
2021-12-14 14:35:02 +0000

    updated score to not change on player moving backwards

--- Assignment/Assets/Scripts/score.cs
+++ Assignment/Assets/Scripts/score.cs
@@ -6,10 +6,16 @@ public class score : MonoBehaviour
 
     public Transform player;
     public Text scoreValue;
+    public float distance;
+    public float previousDistance = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        scoreValue.text = (player.position.x -57.5).ToString("0");
+        distance = player.position.x - 57.5f;
+        if (distance > previousDistance) {
+            scoreValue.text = distance.ToString("0");
+            previousDistance = distance;
+        }
     }
 }
