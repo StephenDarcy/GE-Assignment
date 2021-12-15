1888ed7435655b20a94dd515fcb85ae619f298f0
Stephen Darcy <stephendarcy123@gmail.com>
2021-12-15 12:11:25 +0000

    added comments to jumpStatus.cs

--- Assignment/Assets/Scripts/jumpsStatus.cs
+++ Assignment/Assets/Scripts/jumpsStatus.cs
@@ -11,13 +11,17 @@ public class jumpsStatus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //getting the players jump status
         Player playerScript = player.GetComponent<Player>();
         status = playerScript.jumpIsActivated;
 
+        //if player has jump set to true
         if (status) {
+            //set text to active and set colour
             toggle.text = "Active";
             toggle.color = Color.green;
         } else {
+            //else set text to disabled and change colour to red
             toggle.text = "Disabled";
             toggle.color = Color.red;
         }
